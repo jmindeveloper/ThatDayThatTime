8fb11761a3a01335ce3b6c2c24463e52737d06e7
jmindevelper <jmin.developer@gmail.com>
2022-08-15 13:29:10 +0900

    [feat] sideMenu 생성 #23
    - TimeDiaryVC에서 네비게에션 left button tap시 present

---
+++ ThatDayThatTime/ThatDayThatTime/ViewController/SideMenu/DTSideMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DTSideMenuViewController.swift
+//  ThatDayThatTime
+//
+//  Created by J_Min on 2022/08/15.
+//
+
+import UIKit
+
+final class DTSideMenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .viewBackgroundColor
+    }
+    
+}
