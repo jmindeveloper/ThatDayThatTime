55ebb7bdcd84d36a9ee8f4531ad23c3d5745611c
jmindevelper <jmin.developer@gmail.com>
2022-08-27 13:33:53 +0900

    [feat] 알림문구 설정 #38

--- ThatDayThatTime/ThatDayThatTime/Manager/UserNotificationManager.swift
+++ ThatDayThatTime/ThatDayThatTime/Manager/UserNotificationManager.swift
@@ -33,36 +33,42 @@ final class UserNotificationManager: NSObject {
     func addDefaultNotification() {
         var dateComponents = DateComponents()
         dateComponents = Calendar.current.dateComponents([.hour], from: Date())
+        let subTitles = [
+            "하루가 시작됐습니다",
+            "벌써 하루의 반이 지났네요",
+            "오늘 하루도 잘 보내셨나요?"
+        ]
+        let bodys = [
+            "오늘의 계획이나 목표를 기록해보는건 어떨까요?",
+            "오늘 점심식사는 어땠나요?",
+            "오늘 하루 어떤일이 있었는지 기록해보세요!"
+        ]
+        
         
         defaultIdentifiers.enumerated().forEach { index, identifier in
             let content = UNMutableNotificationContent()
             content.title = "그날 그시간"
+            content.subtitle = subTitles[index]
+            content.body = bodys[index]
             content.sound = .default
             
             switch index {
             case 0:
-                dateComponents.hour = 9
+                dateComponents.hour = 13
             case 1:
                 dateComponents.hour = 13
             case 2:
-                dateComponents.hour = 20
+                dateComponents.hour = 13
             default: break
             }
             let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
             let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
             center.add(request)
         }
-        
-        center.getPendingNotificationRequests { notis in
-            print(notis.count)
-        }
     }
     
     func removeDefaultNotification() {
         center.removePendingNotificationRequests(withIdentifiers: defaultIdentifiers)
-        center.getPendingNotificationRequests { notis in
-            print(notis.count)
-        }
     }
 }
 
