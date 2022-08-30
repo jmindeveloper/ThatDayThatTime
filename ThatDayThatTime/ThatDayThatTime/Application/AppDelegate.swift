8a89e190abe01012fb523596afb3ffa559c811f7
jmindevelper <jmin.developer@gmail.com>
2022-08-30 15:50:52 +0900

    v 1.0

--- ThatDayThatTime/ThatDayThatTime/Application/AppDelegate.swift
+++ ThatDayThatTime/ThatDayThatTime/Application/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UNUserNotificationCenter.current().delegate = self
-        sleep(1)
         return true
     }
 
